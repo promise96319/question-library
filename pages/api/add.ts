22d5534789cbe9f44caa1a7e77a19e48809e6753
guanghui.qin <guanghui.qin@qingteng.cn>
2022-03-25 19:55:29 +0800

    chore: init

---
+++ pages/api/add.ts
@@ -0,0 +1,4 @@
+export default (req, res) => {
+  console.log('req ==> ', req)
+  res.status(200).json({ text: 'Hello' })
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage: React.FC = () => {
+  return (
+    <div>hello world</div>
+  )
+}
+
+export default HomePage
